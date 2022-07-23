cc5c90e36c8bfdffd5e1e34e1c8fedea007de7cc
Leon Erasmus <leondevcode@gmail.com>
2022-07-23 19:21:54 -0400

    react router 6 initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect} from 'react'
+import { Link } from "react-router-dom";
+import React, {useEffect} from 'react';
 
 function App() {
 
@@ -14,20 +14,14 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/hello">hello</Link> |{" "}
+      </nav>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,15 +8,15 @@ const transactions = require('./routes/transactions')
 const PORT = process.env.PORT || 3001
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')))
+/* app.use(express.static(path.resolve(__dirname, '../client/build'))) */
 
 app.use(bodyParser.urlencoded({ extended: true })) //parse body from form
 app.use('/api', transactions)
 
 
-router.get('*', (req, res) =>{
+/* router.get('*', (req, res) =>{
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
-})
+}) */
 
 app.listen(PORT , () => {
     console.log(`Server listening on port: ${PORT}`)
